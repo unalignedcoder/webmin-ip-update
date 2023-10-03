4fcf6e90345043b8e3cd9e517572d30929ec1bc8
unalignedcoder <unalignedcoder@tuscoss.com>
2023-10-03 17:22:44 +0600

    PowerShell script

--- webmin-ip-update.ps1
+++ webmin-ip-update.ps1
@@ -24,14 +24,16 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
 #============== customize variables here
 
-#path to plink executable
+# Path to plink executable
 $plink = "C:\path\to\PLINK.EXE"
 
 # Array of DNS servers to query
 $dnsServers = @("resolver1.opendns.com", "8.8.8.8", "208.67.222.222", "77.88.8.1", "1.1.1.1")
+
+# Get the script's directory
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 # Define the file path for miniserv.conf
 $miniservConfPath = "/etc/webmin/miniserv.conf"
@@ -39,8 +41,10 @@ $miniservConfPath = "/etc/webmin/miniserv.conf"
 # SSH into your remote VPS using the key stored in SSH agent (Pageant)
 $sshHost = "<ip-number or server hostname>"
 $sshUser = "<username with write privileges to miniserv.conf>"
-$sshPort = "<port number>" #usually 22, custom number is recommended
-$hostKey = "<host key fingerprint>" #in the `key-type:host-key` format
+$sshPort = "<port number>" # Usually 22, custom number is recommended
+
+# File to store the last known IP
+$ipStore = Join-Path $scriptDir ".last_known_ip.txt"
 
 #============== end customization
 
@@ -49,53 +53,69 @@ if (-not (Get-Module -Name BurntToast -ListAvailable)) {
     Write-Host "Warning: BurntToast module is not installed. System notifications will not be displayed."
 }
 
-try {	
+try {
+    # Query DNS servers to get the external IP address
+    function Get-ExternalIP {
+        foreach ($dnsServer in $dnsServers) {
+            $externalIP = (Resolve-DnsName -Name myip.opendns.com -Server $dnsServer).IPAddress
+            if ($externalIP) {
+                return $externalIP
+            }
+        }
+        Write-Host "Failed to retrieve external IP"
+    }
 
-	# Query DNS servers to get the external IP address
-	function Get-ExternalIP {
-		foreach ($dnsServer in $dnsServers) {
-			$externalIP = (Resolve-DnsName -Name myip.opendns.com -Server $dnsServer).IPAddress
-			if ($externalIP) {
-				return $externalIP
-			}
-		}
-		Write-Host "Failed to retrieve external IP"
-	}
-	
-	# Call the Get-ExternalIP function to retrieve the external IP
-	$externalIP = Get-ExternalIP
+    # Call the Get-ExternalIP function to retrieve the external IP
+    $externalIP = Get-ExternalIP
 
-	# Export the $currentIP variable and log it to console
-	$env:currentIP = $externalIP
-	Write-Host "Current IP: $env:currentIP"
-	
-	#Use Plink to check miniserv.conf content
-	$miniservConfContent = Invoke-Expression -Command "$plink -ssh $sshUser@$sshHost -hostkey $hostKey -batch -P $sshPort cat $miniservConfPath"
-	
-	# Check if the current IP is already in the miniserv.conf content
-	$ipAlreadyExists = $miniservConfContent -match "allow=$env:currentIP"
-	
-	# If the IP already exists, exit without making changes
-	if ($ipAlreadyExists) {
-		Write-Host "$env:currentIP is already allowed in Webmin. Nothing to do."
-		# Pause execution to keep the window open (debug feature)
-		# Read-Host "Press Enter to exit..."
-		exit
-	}
-			
-	#SSH command
-	$sshEdit = "sed -i 's/^allow=.*$/allow=$env:currentIP/'"
-	
-    # Use Plink to edit miniserv.conf ("&& systemctl restart webmin" probably not needed)
+    # Export the $currentIP variable and log it to console
+    $currentIP = $externalIP
+    Write-Host "Current IP: $currentIP"
+
+    # Read the old IP from the store file, if it exists
+    $oldIP = ""
+    if (Test-Path -Path $ipStore) {
+        $oldIP = Get-Content -Path $ipStore
+    }
+
+    # If the old IP is the same as the current IP, nothing to do
+    if ($oldIP -eq $currentIP) {
+        Write-Host "$currentIP is already allowed in Webmin. Nothing to do."
+        # Pause execution to keep the window open (debug feature)
+        # Read-Host "Press Enter to exit..."
+        exit
+    }
+
+    # SSH command to edit miniserv.conf
+    $sshEdit = "sed -i 's/^allow=.*$/allow=$currentIP/'"
+
+    # Use Plink to check miniserv.conf content
+    $miniservConfContent = Invoke-Expression -Command "$plink -ssh $sshUser@$sshHost -hostkey $hostKey -batch -P $sshPort cat $miniservConfPath"
+
+    # Check if the current IP is already in the miniserv.conf content
+    $ipAlreadyExists = $miniservConfContent -match "allow=$currentIP"
+
+    # If the IP already exists, remove it
+    if ($ipAlreadyExists) {
+        $miniservConfContent = $miniservConfContent -replace "allow=$currentIP", ""
+    }
+
+    # Append the new IP to the miniserv.conf content
+    $miniservConfContent += "allow=$currentIP"
+
+    # Use Plink to update miniserv.conf
     Start-Process -FilePath "$plink" -ArgumentList "-ssh $sshUser@$sshHost -hostkey $hostKey -batch -P $sshPort ""$sshEdit"" $miniservConfPath" -NoNewWindow -Wait
+
+    # Update the IP store file with the current IP
+    $currentIP | Set-Content -Path $ipStore
 
     # Check if BurntToast is installed before attempting to display notification
     if (Get-Module -Name BurntToast -ListAvailable) {
-		New-BurntToastNotification -Text "IP address updated successfully.`nNew IP: $externalIP" -AppLogo "ip.png"
-		} 
-	#show console message anyhow
-	Write-Host "Notification: IP address updated successfully. New IP: $externalIP"
-	
+        New-BurntToastNotification -Text "IP address updated successfully.`nNew IP: $currentIP" -AppLogo "ip.png"
+    }
+
+    Write-Host "Notification: IP address updated successfully. New IP: $currentIP"
+
 } catch {
 	
     # Handle errors
@@ -113,4 +133,4 @@ try {
 } 
 
 # Pause execution to keep the window open (debug feature)
-# Read-Host "Press Enter to exit..."
+ Read-Host "Press Enter to exit..."
